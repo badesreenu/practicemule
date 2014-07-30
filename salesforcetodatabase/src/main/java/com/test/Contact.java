2327bcda8e79068ae1ca9a2fbc9f972cec27a337
badesreenu <sreenivasa.bade@whishworks.com>
2014-07-30 12:56:35 +0530

    Added Projects

---
+++ salesforcetodatabase/src/main/java/com/test/Contact.java
@@ -0,0 +1,61 @@
+package com.test;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name = "contact")
+public class Contact implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@XmlAttribute(name = "email")
+	private String email;
+
+	@XmlAttribute(name = "first_name")
+	private String first_name;
+	
+	@XmlElement(name = "last_name")
+	private String last_name;
+	
+	@XmlElement(name = "lastModifiedDate")
+	private String lastModifiedDate;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(String lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+
+}
